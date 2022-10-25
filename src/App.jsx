1d8f8d4187715b4625bf704ce6b2afbbb5226e3c
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-10-25 20:22:04 +0300

    add colorpicker

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import { ColorPicker } from "./components/ColorPicker";
+
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <ColorPicker options={colorPickerOptions} />
+      </>
+    );
+  }
+}
